40fdc2d65942fb3ac4617f5147834737cc25c238
naveen prakash <naveenprakashd@gmail.comgit onfig --global user.email naveenprakashd@gmail.com>
2019-08-09 10:11:35 +0530

    config-server-micro-services

---
+++ src/main/java/com/stackroute/microservice/configserve/ConfigServeApplication.java
@@ -0,0 +1,15 @@
+package com.stackroute.microservice.configserve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServeApplication.class, args);
+	}
+
+}
